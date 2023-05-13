46cad5fa3da70017e0a018dce87d457a933aca57
Vinicius Pacheco <viniciusgrp@gmail.com>
2023-05-13 15:08:11 -0300

    fix change API url to deploy

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3000",
-    // baseURL: "https://stockcontrol.onrender.com",
+    // baseURL: "http://localhost:3000",
+    baseURL: "https://stockcontrol.onrender.com",
     timeout: 5000
 });
